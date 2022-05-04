40ef62699a03da2db3248d1b4533a1be58b9d8a4
Uchida Ryo <uchidaryo.gm@gmail.com>
2022-05-04 11:02:21 +0900

    add: Hello worldのためのソースファイル追加とビルド設定変更

---
+++ src/hello.cpp
@@ -0,0 +1,11 @@
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char** argv) {
+    rclcpp::init(argc, argv);
+
+    auto node = rclcpp::Node::make_shared("hello");
+    RCLCPP_INFO(node->get_logger(), "Hello, ROS2 world!");
+
+    rclcpp::shutdown();
+    return 0;
+}
