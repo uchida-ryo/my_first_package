d9568a862770880df754dd2f3a8ebdcc10ed365a
Uchida Ryo <uchidaryo.gm@gmail.com>
2022-05-05 21:50:46 +0900

    update: 中身スリープのみのメインループを追加

--- src/hello.cpp
+++ src/hello.cpp
@@ -7,6 +7,11 @@ int main(int argc, char **argv)
   auto node = rclcpp::Node::make_shared("hello");
   RCLCPP_INFO(node->get_logger(), "Hello, ROS2 world!");
 
+  rclcpp::WallRate loop(1);
+  while (rclcpp::ok()) {
+    loop.sleep();
+  }
+
   rclcpp::shutdown();
   return 0;
 }
