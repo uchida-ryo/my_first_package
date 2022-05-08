9d698bd652089f5cf6760fc7296e80e6328a669c
Uchida Ryo <uchidaryo.gm@gmail.com>
2022-05-08 21:20:57 +0900

    style: コーディングスタイルガイドを適用

--- src/my_subscriber.cpp
+++ src/my_subscriber.cpp
@@ -8,15 +8,15 @@ void callback(const std_msgs::msg::String::SharedPtr msg)
   RCLCPP_INFO(g_node->get_logger(), "%s", msg->data.c_str());
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    rclcpp::init(argc, argv);
-    g_node = rclcpp::Node::make_shared("my_subscriber");
-    auto subscriber = g_node->create_subscription<std_msgs::msg::String>("greeting", 1, callback);
+  rclcpp::init(argc, argv);
+  g_node = rclcpp::Node::make_shared("my_subscriber");
+  auto subscriber = g_node->create_subscription<std_msgs::msg::String>("greeting", 1, callback);
 
-    rclcpp::spin(g_node);
+  rclcpp::spin(g_node);
 
-    g_node = nullptr;
-    rclcpp::shutdown();
-    return 0;
+  g_node = nullptr;
+  rclcpp::shutdown();
+  return 0;
 }
